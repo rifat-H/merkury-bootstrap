a4799efde885c457a06efa01cd73a7b7d52c3e9b
Joy Mia <joymia74@gmail.com>
2024-06-15 00:23:33 +0600

    init

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+import "../scss/bundle.scss";
+
+
+window.$ = window.jQuery = require('jquery');
+require('jquery-migrate');
+require('slick-carousel');
+require('popper.js');
+require('bootstrap');
+require('@fortawesome/fontawesome-free/js/all');
+require('nestable2');
